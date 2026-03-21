3dfff1d6d6dd63ab2dd99335a93a438db0b2e932
Nawaz Gafar <gafar.nawaz@gmail.com>
2026-03-21 10:58:41 -0400

    Refactored base36 encoding to use a constant for characters

--- django_puid/utils.py
+++ django_puid/utils.py
@@ -1,20 +1,20 @@
 import secrets
 import time
 
+BASE36_CHARS = "0123456789abcdefghijklmnopqrstuvwxyz"
+
 
 def base36_encode(number: int) -> str:
     if number == 0:
         return "0"
-    chars = "0123456789abcdefghijklmnopqrstuvwxyz"
     base36 = ""
     while number:
         number, i = divmod(number, 36)
-        base36 = chars[i] + base36
+        base36 = BASE36_CHARS[i] + base36
     return base36
 
 
 def generate_id(prefix: str, random_length: int = 6, separator: str = "_") -> str:
     timestamp_b36 = base36_encode(int(time.time() * 1000))
-    chars = "0123456789abcdefghijklmnopqrstuvwxyz"
-    random_part = "".join(secrets.choice(chars) for _ in range(random_length))
+    random_part = "".join(secrets.choice(BASE36_CHARS) for _ in range(random_length))
     return f"{prefix}{separator}{timestamp_b36}{random_part}"
